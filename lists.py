e5af3a5f81be08ef28600a3d392a82e581ceb231
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-07-13 10:16:10 +0300

    lists

--- lists.py
+++ lists.py
@@ -4,6 +4,17 @@
 # 3- List elements do not need to be unique
 # 4- Different data types
 
-mylist = []
-mylist.append(1)
-print(type(mylist), mylist)
+myList = [1, 2, 3, 4, 5]
+otherList = [6, 7, 8]
+myList.extend(otherList)
+
+import sys
+
+n = 30
+myDynamicArray = []
+
+for i in range(0, n):
+    myLength = len(myDynamicArray)
+    myByte = sys.getsizeof(myDynamicArray)
+    print(f"Length: {myLength} Byte: {myByte}")
+    myDynamicArray.append(n)
