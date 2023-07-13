0760c25e03862051a9da19b086ad8089ed84bb81
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-07-13 09:34:57 +0300

    lists

---
+++ lists.py
@@ -0,0 +1,9 @@
+# What Is a List in Python?
+# 1- Lists are ordered
+# 2- Lists are mutable
+# 3- List elements do not need to be unique
+# 4- Different data types
+
+mylist = []
+mylist.append(1)
+print(type(mylist), mylist)
