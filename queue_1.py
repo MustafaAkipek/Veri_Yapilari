573e588237cceafad73825518d6e64797b0a73f7
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-07-17 10:03:49 +0300

    queue

---
+++ queue_1.py
@@ -0,0 +1,15 @@
+# Queue
+# Listeye benzer ama ilk giren ilk çıkar kuralı vardır(FIFO)
+# Queue' ye eleman eklemek için Enqeueu
+# Queue' den eleman kaldırmak için Dequeue
+# Front Queue' nin ilk girenler kısmı, Rear sondan girenler kısmı
+
+from queue import Queue
+
+myQueue = Queue()
+myQueue.put(1) # Enqueue işlemi yapıp Queue' ye 1 elemanını ekledik
+myQueue.put(10) # Enqueue işlemi yapıp Queue' ye 10 elemanını ekledik
+myQueue.put(20) # Enqueue işlemi yapıp Queue' ye 20 elemanını ekledik
+print(myQueue.get()) # Dequeue işlemi yapıp Queue' nun ilk elemanı kaldırdık(1)
+print(myQueue.get()) # Dequeue işlemi yapıp Queue' nun ilk elemanı kaldırdık(10)
+print(myQueue.get()) # Dequeue işlemi yapıp Queue' nun ilk elemanı kaldırdık(20)
