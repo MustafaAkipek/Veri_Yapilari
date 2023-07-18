a596cf7d7efa471c9703717220429a31b68d7d96
MustafaAkipek <101327731+MustafaAkipek@users.noreply.github.com>
2023-07-18 10:35:13 +0300

    queue coding

--- queue_kodlama.py
+++ queue_kodlama.py
@@ -22,3 +22,14 @@ class Queue():
     
     def size(self):
         return len(self.elements)
+    
+myQueue = Queue()
+myQueue.isEmpty()
+myQueue.enqueue(10) # append 10
+myQueue.enqueue(20) # append 20
+myQueue.enqueue(30) # append 30
+myQueue.dequeue() # remove 10
+myQueue.size() # 2
+myQueue.dequeue() # remove 20
+myQueue.dequeue() # remove 30
+a = myQueue.isEmpty() # True
